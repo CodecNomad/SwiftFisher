80445496da866b570b0023a778b264642a1b8328
codec <codec@github.com>
2024-05-09 11:10:44 +0200

    Temporary push

--- src/main/kotlin/com/github/codecnomad/swiftfisher/SwiftFisher.kt
+++ src/main/kotlin/com/github/codecnomad/swiftfisher/SwiftFisher.kt
@@ -1,22 +1,13 @@
 package com.github.codecnomad.swiftfisher
 
-import com.github.codecnomad.swiftfisher.util.rotation.RotationManager
-import net.minecraft.util.BlockPos
-import net.minecraftforge.client.event.ClientChatReceivedEvent
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.event.FMLInitializationEvent
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 @Mod(modid = "swiftfisher", useMetadata = true)
 class SwiftFisher {
     @Mod.EventHandler
     fun init(event: FMLInitializationEvent) {
         MinecraftForge.EVENT_BUS.register(this)
-    }
-
-    @SubscribeEvent
-    fun onChat(event: ClientChatReceivedEvent) {
-        if (event.message.toString().contains("x!")) RotationManager.rotateTo(BlockPos(3, 6, 3))
     }
 }
--- src/main/kotlin/com/github/codecnomad/swiftfisher/macro/MacroManager.kt
+++ src/main/kotlin/com/github/codecnomad/swiftfisher/macro/MacroManager.kt
@@ -2,15 +2,26 @@ package com.github.codecnomad.swiftfisher.macro
 
 import net.minecraft.client.Minecraft
 import net.minecraft.client.entity.EntityPlayerSP
+import net.minecraft.client.settings.KeyBinding
+import net.minecraft.entity.Entity
+import net.minecraft.entity.item.EntityArmorStand
+import net.minecraft.entity.projectile.EntityFishHook
 import net.minecraft.item.ItemFishingRod
 import net.minecraft.item.ItemStack
+import net.minecraft.world.World
 import net.minecraftforge.common.MinecraftForge
+import net.minecraftforge.event.entity.EntityJoinWorldEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent
+import java.lang.Thread.sleep
 
 class MacroManager {
     private var phase: Phase = Phase.SETUP_FISHING_ROD
-    private var player: EntityPlayerSP = Minecraft.getMinecraft().thePlayer
+    private var entityHook: EntityFishHook? = null
+    private var entityMarker: EntityArmorStand? = null
+    private var entityMonster: Entity? = null
+    private val player: EntityPlayerSP = Minecraft.getMinecraft().thePlayer
+    private val world: World = Minecraft.getMinecraft().theWorld
 
     fun enable() {
         phase = Phase.SETUP_FISHING_ROD
@@ -18,9 +29,10 @@ class MacroManager {
         MinecraftForge.EVENT_BUS.unregister(this)
     }
 
-    fun disable() {
+    private fun disable() {
         MinecraftForge.EVENT_BUS.unregister(this)
     }
+
 
     @SubscribeEvent
     fun onTick(event: ClientTickEvent) {
@@ -37,27 +49,62 @@ class MacroManager {
                     break
                 }
 
-                if (!hasFound)
-                    disable()
+                if (!hasFound) return disable()
 
-                phase = Phase.WAIT_FOR_MOB
+                KeyBinding.onTick(Minecraft.getMinecraft().gameSettings.keyBindUseItem.keyCode)
+                phase = Phase.WAIT_FOR_CATCH
             }
 
-            Phase.WAIT_FOR_MOB -> {
+            Phase.WAIT_FOR_CATCH -> {
+                callbackIn(150) {
 
-                phase = Phase.KILL_MOB
+                    if (entityHook == null || entityHook!!.isDead) {
+                        for (entity: Entity in world.loadedEntityList) {
+                            if (entity !is EntityFishHook) continue
+                            entityHook = entity
+                        }
+                    }
+
+                    if (entityMarker != null && !entityMarker!!.isDead) {
+                        KeyBinding.onTick(Minecraft.getMinecraft().gameSettings.keyBindUseItem.keyCode)
+                        phase = Phase.KILL_MOB
+                    } else
+                        phase = Phase.WAIT_FOR_CATCH
+                }
             }
 
             Phase.KILL_MOB -> {
 
-                phase = Phase.SETUP_FISHING_ROD
+            }
+
+            Phase.SUSPEND -> {
+
             }
         }
     }
 
     enum class Phase {
-        SETUP_FISHING_ROD,
-        WAIT_FOR_MOB,
-        KILL_MOB
+        SETUP_FISHING_ROD, WAIT_FOR_CATCH, KILL_MOB, SUSPEND
+    }
+
+    private fun callbackIn(ms: Long, callback: Runnable) {
+        phase = Phase.SUSPEND
+
+        Thread {
+            sleep(ms)
+            callback.run()
+        }.start()
+    }
+
+    @SubscribeEvent
+    fun onEntitySpawn(event: EntityJoinWorldEvent) {
+        if (entityHook == null || /*event.entity is EntitySquid ||*/ event.entity.name.equals("item.tile.stone.stone")) return
+
+        // The countdown that shows up when entity is starting to bite
+        if (event.entity is EntityArmorStand && event.entity.getDistanceToEntity(entityHook) < 0.1) entityMarker =
+            event.entity as EntityArmorStand
+        // The entity that comes at you when you catch smt
+        else if (event.entity.getDistanceToEntity(entityHook) <= 1.5) entityMonster = event.entity as Entity
+
     }
 }
